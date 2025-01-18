028c6a816df4e67d90f4a52cdbaa47445d4d08c4
Peter Krive <thepetruha@Peters-MacBook-Pro.local>
2025-01-18 22:24:08 +0400

    fix:v2 Fix redis clinet config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -13,6 +13,8 @@ export default {
     slinportApiClientSecret: process.env.SKINPORT_API_CLIENT_SECRET,
     redisUser:  process.env.REDIS_USER,
     redisPassword:  process.env.REDIS_PASSWORD,
+    redisHost: "127.0.0.1",
+    redisPort: 6379, 
     postgresUser:  process.env.POSTGRES_USER,
     postgresPassword:  process.env.POSTGRES_PASSWORD,
     postgresPort: 5432,
