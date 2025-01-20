dda2d67aecf53756855d1db859d54544199f647b
Peter Krive <thepetruha@Peters-MacBook-Pro.local>
2025-01-20 18:47:49 +0400

    fix: Fix cache for skinport items

--- src/api/skinport.ts
+++ src/api/skinport.ts
@@ -1,8 +1,35 @@
 import HTTP from "../utils/http";
 
+export type TypeCurrencySkinport = "AUD" | "BRL" | "CAD" | "CHF" | "CNY" | "CZK" | "DKK" | "EUR" | "GBP" | "HRK" | "NOK" | "PLN" | "RUB" | "SEK" | "TRY" | "USD";
+
+export type TypeCurrenctySP = TypeCurrencySkinport | TypeCurrencySkinportEnum | string;
+
+export enum TypeCurrencySkinportEnum {
+    AUD = "AUD",
+    BRL = "BRL",
+    CAD = "CAD",
+    CHF = "CHF",
+    CNY = "CNY",
+    CZK = "CZK",
+    DKK = "DKK",
+    EUR = "EUR",
+    GBP = "GBP",
+    HRK = "HRK",
+    NOK = "NOK",
+    PLN = "PLN",
+    RUB = "RUB",
+    SEK = "SEK",
+    TRY = "TRY",
+    USD = "USD",
+}
+  
+export function isValidCurrency(currency: string): currency is TypeCurrencySkinport {
+    return Object.values(TypeCurrencySkinportEnum).includes(currency as TypeCurrencySkinportEnum);
+}
+
 export interface ISkinportItem {
     market_hash_name: string;
-    currency: "USD" | "EUR" | string;
+    currency: TypeCurrenctySP; 
     suggested_price: number;
     item_page: string; 
     market_page: string;
@@ -39,11 +66,15 @@ class Skinport extends HTTP {
         return this.instance;
     }
 
-    public async getItems(currency: "USD" | "EUR"): Promise<{ status: number, body: ISkinportItem[] }> {
-        const endpoint = "/v1/items" + `?currency=${currency}` + `&tradable=${true}`;
+    public async getItems(appId: number, currency: TypeCurrenctySP = "EUR"): Promise<{ status: number, body: ISkinportItem[] }> {
+        const endpoint = "/v1/items" + 
+            `?app_id=${appId}` + 
+            `&currency=${currency}`;
+
         const { body, status } = await this.request(endpoint, {
             headers: { "Accept-Encoding": "br" }
         });
+
         return { body: body as ISkinportItem[], status };
     }
 
--- src/handlers/skinport.ts
+++ src/handlers/skinport.ts
@@ -1,8 +1,31 @@
 import { Request, Response } from "express";
 import Handler, { Status } from ".";
-import Skinport, { ISkinportItem } from "../api/skinport";
+import Skinport, { ISkinportItem, isValidCurrency } from "../api/skinport";
 import RedisClient from "../redis";
 import Redis from "ioredis";
+
+
+function getItemsWithMinPrices(items: ISkinportItem[]): Array<{ tradableMin: number; nonTradableMin: number }> {
+    return items.map(item => {
+        const tradablePrices = [item.min_price, item.max_price, item.mean_price, item.median_price]
+            .filter(price => price !== null) as number[];
+        const tradableMin = Math.min(...tradablePrices);
+    
+        const nonTradablePrices = [item.suggested_price]
+            .filter(price => price !== null) as number[];
+        const nonTradableMin = Math.min(...nonTradablePrices);
+    
+        return {
+            marketName: item.market_hash_name,
+            createAt: item.created_at,
+            updatedAt: item.updated_at,
+            quantity: item.quantity,
+            currency: item.currency,
+            tradableMin,
+            nonTradableMin,
+        };
+    });
+}
 
 export default class SkinportHandler extends Handler {
     private skinportApi: Skinport;
@@ -16,20 +39,34 @@ export default class SkinportHandler extends Handler {
 
     public async getItems(req: Request, res: Response) {
         try {
-            const cacheKey = `items:tradable:true`;
+            const { app_id = 730, currency = "EUR" } = req.query || {};
+
+            const inAppId = Number(app_id);
+
+            if (Number.isNaN(inAppId)) {
+                throw new Error("Incorrect parametrs - appId");
+            }
+
+            const cacheKey = `items:${app_id}:currncy:${currency}`;
             const cachedData = await this.redisClient.get(cacheKey);
 
             if (cachedData) {
                 const parsedCachedData = JSON.parse(cachedData) as ISkinportItem[];
                 this.response(res, Status.Ok, parsedCachedData)
                 return;
+            } 
+
+            const isParamCurrncy = isValidCurrency(String(currency));
+            if (!isParamCurrncy) {
+                throw new Error("Incorrect parametrs - currncy");
             }
 
-            const { body } = await this.skinportApi.getItems("USD"); 
-            await this.redisClient.setex(cacheKey, 5 * 1000 * 60, JSON.stringify(body));
+            const { body } = await this.skinportApi.getItems(inAppId, String(currency));
+            const processedItems = getItemsWithMinPrices(body);
+            await this.redisClient.setex(cacheKey, 5 * 1000 * 60, JSON.stringify(processedItems));
             
             res.setHeader("Cache-Control", "max-age=60000")
-            this.response(res, Status.Ok, body);
+            this.response(res, Status.Ok, processedItems);
         } catch (error) {
             console.error("Error in handleCreate:", error);
             res.status(500).json({ message: "Internal server error" });
